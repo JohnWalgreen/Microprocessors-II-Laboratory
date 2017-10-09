9fde74a17d90a1c620bd3e89626e7d9abfc46716
HansHoene <31940492+HansHoene@users.noreply.github.com>
2017-10-09 15:26:35 -0400

    Create main_v0.c

---
+++ Lab 2/main_v0.c
@@ -0,0 +1,17 @@
+/*
+File Created On: 09-Oct-2017
+Release Date (successful compile and run): TBD
+
+Description:
+	This file is the first main program for Lab 2.  
+This program will be designed to match the specifications 
+layed out in the <<provided materials>> folder, and this 
+program will most likely resemble the skeleton code in 
+<<my-skeleton.c>>.
+	
+Edits:
+Initials			Date				Description
+-------------------------------------------------------
+HEH					09-Oct-2017
+
+*/
