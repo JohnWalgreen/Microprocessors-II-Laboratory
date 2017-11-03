b74215c8ec4814407bde27b97cb4c77c709880d4
HansHoene <31940492+HansHoene@users.noreply.github.com>
2017-11-03 13:16:00 -0400

    I do not know the rest of the lyrics

--- Lab 3/galileo code/main.c
+++ Lab 3/galileo code/main.c
@@ -31,8 +31,49 @@ Lab objectives from provided materials:
 #include <linux/i2c-dev.h>		// access i2c adapter from linux program; this may be incorrect library
 #define ADAPTER_NUMBER 0		// is determined dynamically [inspect /sys/class/i2c-dev/ or run "i2cdetect -l" to decide this.]
 
+/*STUFF I STOLE FROM INTERNET*/
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/*END*/
 
 int main() {
+
+	// get temperature from TMP102
+	int handle;
+	int address;
+	char buf[10] = { 0 };
+	float data;
+	char channel;
+
+	address = 0x48;
+	handle = openDevice(0);
+	ioctl(handle, I2C_SLAVE, address);
+
+	for (int i = 0; i<4; i++) {     // Using I2C Read
+		if (read(file,buf,2) != 2) {
+			/* ERROR HANDLING: i2c transaction failed */
+			printf("Failed to read from the i2c bus.\n");
+			buffer = g_strerror(errno);
+			printf(buffer);
+			printf("\n\n");
+		} else {
+			data = (float)((buf[0] & 0b00001111)<<8)+buf[1];
+			data = data/4096*5;
+			channel = ((buf[0] & 0b00110000)>>4);
+			printf("Channel %02d Data:  %04f\n",channel,data);
+		}
+	}
+
 
 	/*declare and initialise variables here*/
 
