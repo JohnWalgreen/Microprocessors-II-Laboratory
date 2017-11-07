5b0afb8e1b78e3c79ba8a585c5f003c18b293488
HansHoene <31940492+HansHoene@users.noreply.github.com>
2017-11-07 14:53:22 -0500

    updates on acquire and sample runs

--- Lab 3/to Galileo/acquire.c
+++ Lab 3/to Galileo/acquire.c
@@ -20,15 +20,17 @@
 #include <fcntl.h>
 /*END*/
 
+#include <time.h>
+
 /*COPIED*/
 #include <sys/ioctl.h>
 
 //*** Opencv libraries ***
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+//#include <opencv2/objdetect/objdetect.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv/cv.h>
+//#include <opencv/highgui.h>
 
 //*** Normal c/c++ code libraries ***
 //#include <fcntl.h>
@@ -54,6 +56,9 @@
 
 #include "i2c.h"
 
+#define DELTA_T 5
+#define DELAY 0
+#define ROLLOVER 100
 
 int main() {
 
@@ -61,24 +66,30 @@ int main() {
 	char buffer[200];
 	FILE *file;
 	unsigned int i, j, counter;
-	double t;
+	double t1, t2;
+	clock_t oldstart, start, end;
 
 	handle = InitTempDevice(ADAPTER_NUMBER);
 
 	// measure noise
 	printf("Keep your filthy paws away from the sensor.");
-	sprint(buffer, "%s/noise.csv", DEST_FOLDER);
+	fflush(stdout);
+	sprintf(buffer, "%s/noise.csv", DEST_FOLDER);
 	file = fopen(buffer, "w");
 	if (file == NULL) {
 		fputs("\nError opening noise.csv", stderr);
 		return 0;
 	}
 	// 10 seconds / 10 000 iterations = 1/1000 seconds per iteration or 1 ms per iteration
-	fprintf("time (seconds),temperature (Celsius)\n");
-	for (i = 0; i < 10000; i++) {
-		fprintf(file, "%u,%2.4lf\n", (double)i / 1000, readTemp(handle));
-		usleep(1000);
-	}
+	fprintf(file, "time (seconds),temperature (Celsius)\n");
+	start = clock();
+	do {
+		end = clock();
+		if ((end - start) % ROLLOVER == 0) {
+			fprintf(file, "%.3lf,%2.4lf\n", (double)(end - start) / (double)CLOCKS_PER_SEC, readTemp(handle));
+		}
+		usleep(DELAY);
+	} while (end - start < DELTA_T * CLOCKS_PER_SEC);
 	fclose(file);
 	sleep(5);
 
@@ -89,15 +100,22 @@ int main() {
 		fputs("\nError while opening infrequent taps.csv", stderr);
 	}
 	printf("\rOkay fucktard, get ready.  When I say \"GO\", gently tap the sensor every two seconds until I tell you to stop.");
+	fflush(stdout);
 	sleep(10);
 	printf("\rGO!!!!!");
+	fflush(stdout);
 	sleep(3);
-	fprintf("time (seconds),temperature (Celsius)\n");
-	for (i = 0; i < 10000; i++) {
-		fprintf(file, "%u,%2.4lf", (double)i / 1000, readTemp(handle));
-		usleep(1000);//1 ms
-	}
-	printf("/rSTOP!");
+	fprintf(file, "time (seconds),temperature (Celsius)\n");
+	start = clock();
+	do {
+		end = clock();
+		if ((end - start) % ROLLOVER == 0) {
+			fprintf(file, "%.3lf,%2.4lf\n", (double)(end - start) / (double)CLOCKS_PER_SEC, readTemp(handle));
+		}
+		usleep(DELAY);
+	} while (end - start < DELTA_T * CLOCKS_PER_SEC);
+	printf("\rSTOP!");
+	fflush(stdout);
 	fclose(file);
 	sleep(5);
 
@@ -109,15 +127,22 @@ int main() {
 		return 0;
 	}
 	printf("\rOkay asshole, when I say \"GO\", gently tap the sensor once per second until I tell you to stop.");
+	fflush(stdout);
 	sleep(10);
 	printf("\rGO!!!!!!!!!!!!!");
+	fflush(stdout);
 	sleep(3);
-	fprintf("time (seconds),temperature (Celsius)\n");
-	for (i = 0; i < 10000; i++) {
-		fprintf(file, "%u,%2.4lf", (double)i / 1000, readTemp(handle));
-		usleep(1000);//1 ms
-	}
+	fprintf(file, "time (seconds),temperature (Celsius)\n");
+	start = clock();
+	do {
+		end = clock();
+		if ((end - start) % ROLLOVER == 0) {
+			fprintf(file, "%.3lf,%2.4lf\n", (double)(end - start) / (double)CLOCKS_PER_SEC, readTemp(handle));
+		}
+		usleep(DELAY);
+	} while (end - start < DELTA_T * CLOCKS_PER_SEC);
 	printf("/rSTOP!");
+	fflush(stdout);
 	fclose(file);
 	sleep(5);
 
@@ -125,37 +150,45 @@ int main() {
 	sprintf(buffer, "%s/long presses.csv", DEST_FOLDER);
 	file = fopen(buffer, "w");
 	if (file == NULL) {
-		fputs("Error opening long presses.csv", DEST_FOLDER);
+		fputs("Error opening long presses.csv", stderr);
 		return 0;
 	}
-	fprintf("time (seconds),temperature (Celsius),touching?\n");
+	fprintf(file, "time (seconds),temperature (Celsius),touching?\n");
 	printf("\rNow pay attention.\nWhen I say \"GO\", put your finger on the sensor.\nWhen I say \"STOP\", take it off the sensor.\nThis will repeat multiple times, so when I say \"DONE\", the test is over.\nIt is extremely important that you follow directions as promptly as possible.\n\n");
+	fflush(stdout);
 	sleep(20);
 	printf("Get ready");
 	sleep(5);
 	printf("\rGET SET...");
 	sleep(5);
 
-	counter = 0;
-	for (j = 1; j <= 10; j++) {
-		t = readTemp(handle);
+	oldstart = clock();
+	for (j = 1; j <= DELTA_T; j++) {
+		t1 = readTemp(handle);
 		printf("\rGO!");
-		for (i = 0; i < (j * 1000); i++) {
-			fprintf(file, "%u,%2.4lf,1", (double)counter / 1000, readTemp(handle));
-			++counter;
-			usleep(1000);//1 ms
-		}
-		t = ((1 - 0.9) * (readTemp(handle) - t)) + t;		// 90% cooling
+		fflush(stdout);
+		start = clock();
+		do {
+			end = clock();
+			fprintf(file, "%.3lf,%2.4lf\n", (double)(end - oldstart) / (double)CLOCKS_PER_SEC, readTemp(handle));
+			usleep(DELAY);
+		} while (end - start < j * CLOCKS_PER_SEC);
+
+		t1 = ((1 - 0.9) * (readTemp(handle) - t1)) + t1;		// 90% cooling
 		printf("\rSTOP! Cooling right now. Wait.");
+		fflush(stdout);
 		
-		while (readTemp(handle) > t) {
-			fprintf(file, "%u,%2.4lf,0", (double)counter / 1000, readTemp(handle));
-			++counter;
-			usleep(1000);//1 ms
-		}
+		start = clock();
+		do {
+			end = clock();
+			t2 = readTemp(handle);
+			fprintf(file, "%.3lf,%2.4lf\n", (double)(end - oldstart) / (double)CLOCKS_PER_SEC, t2);
+			usleep(DELAY);
+		} while (t2 > t1);
 	}
 
 	printf("\rDONE.\n\nThank you for doing this test.\nMove all CSV files to a PC and process data.\n\n");
+	fflush(stdout);
 	fclose(file);
 
 	return 0;
